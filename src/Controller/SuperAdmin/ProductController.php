c0d68c25a286162b46a92f8379bae76c22121373
John Doe <johndoe@example.com>
2020-01-30 19:13:13 +0100

    Function add & delete product

---
+++ src/Controller/SuperAdmin/ProductController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\SuperAdmin;
+
+use App\Entity\Product;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\ConstraintViolationList;
+use FOS\RestBundle\Controller\Annotations\Delete;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+class ProductController extends AbstractFOSRestController
+{
+    /**
+     * @Post(
+     *    path = "/admin/creer-produit",
+     *    name = "admin_app_product_create"
+     * )
+     * @View(StatusCode = 201)
+     * @ParamConverter(
+     *     "product",
+     *     converter="fos_rest.request_body"
+     * )
+     */
+    public function addProduct(Product $product, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            return $this->view($violations, Response::HTTP_BAD_REQUEST);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
+
+        return $this->view($product, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Delete("/admin/supprimer-produit/{id}", name="admin_app_product_delete", requirements = {"id"="\d+"})
+     * @View(StatusCode = 200)
+     */
+    public function DeleteProduct(Product $product)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($product);
+        $em->flush();
+
+        return $this->view(null, Response::HTTP_OK);
+    }
+}
